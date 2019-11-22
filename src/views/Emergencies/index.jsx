f8c1fc07f01ddb9ce9d1de5a9779e6e31f50b470
YongmingLi <yongming.li@sjsu.edu>
2019-11-22 13:41:04 -0800

    energency page update

--- src/views/Emergencies/index.jsx
+++ src/views/Emergencies/index.jsx
@@ -117,8 +117,20 @@ class Emergencies extends Component {
     const { emergencies } = this.state;
 
     return (
+
       <DashboardLayout title="Emergencies">
         <div className={classes.root}>
+        <Grid container spacing={4}>
+          <Grid item lg={6} sm={6} xl={6} xs={12}>
+            <EmergenciesInProgress className={classes.item} />
+          </Grid>
+          <Grid item lg={6} sm={6} xl={6} xs={12}>
+            <EmergenciesResolved className={classes.item} />
+          </Grid>
+          <Grid item lg={12} md={12} xl={12} xs={12}>
+            {this.renderEmergencies()}
+          </Grid>
+        </Grid>
           <EmergencyToolbar
             createEmergency={this.addEmergency.bind(this)}
             search={this.fetchEmergencies.bind(this)}
